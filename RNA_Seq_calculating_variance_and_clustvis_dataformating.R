3d6654053a5590257461f963e77d825a70cfddc6
przybyllab <146016089+przybyllab@users.noreply.github.com>
2024-01-15 12:36:03 -0600

    Update RNA_Seq_calculating_variance_and_clustvis_dataformating.R
    corrected and reformatted the comments

--- RNA_Seq_calculating_variance_and_clustvis_dataformating.R
+++ RNA_Seq_calculating_variance_and_clustvis_dataformating.R
@@ -8,15 +8,15 @@ library(clustvis)
 CalVar <- function(df) {
   
   #converting all beta values to numeric type as they are read as a character when read from a csv
-  #3 IS HARD CODED, THIS NEEDS TO BE CHANGED IF THE DESCRIPTION DATA IS CHANGED
+  #3 is hard coded, this needs to be changed f the description data is changed
   valuesAsNumeric = mutate_all(df[3:dim(df)[1],], function(x) as.numeric(as.character(x)))
   variance = rowVars(as.matrix(valuesAsNumeric)) #calculating the variation 
   
   #adding the beta values variation
-  #the length of the beta_variance vector is 518 433 and will be adding it as a column to the dataframe data_of_interest,
-  #therefore the vector has to have the same length are there are number of rows, so we have to add 
+  #the beta_variance vector will be adding as a column to the dataframe data_of_interest,
+  #therefore the vector has to have the same length as there are number of rows, so we have to add 
   #null values so that the vector has the correct length
-  #I will be adding -1s because variance can not be a -1 value and we are selecting for the largest variance values
+  #-1s  will be added because variance can not be a -1 value
   rowOfData_of_interest =  dim(df)[1]
   NumberOfNullValuesToAdd = rowOfData_of_interest - length(variance)
   variance_column = c( rep(-1, each= NumberOfNullValuesToAdd), variance) # concatenating 26 -1s 
@@ -24,35 +24,6 @@ CalVar <- function(df) {
   return(df)
 }
 
-
-#Creating a function to take the top 5% variation beta values 
-top5Percent_Var <- function(df){
-  
-  #note that because the first rows of our dataframe hold description data, they will have a -1 beta_variation value
-  variance_column = df$variance #getting a vector of beta variation
-  Is_neg1 = variance_column == -1 
-  descriptionRowsCutOff = match(FALSE, Is_neg1 ) - 1 
-  
-  #separating the descriptor rows from RNAseq vsd rows 
-  designData = df[1:descriptionRowsCutOff,]
-  last_row = dim(df)[1]
-  RNAseq = df[descriptionRowsCutOff+1 : last_row,]
-  
-  #sorting by variance
-  sortedVarValues <- RNAseq [order(RNAseq$variance, decreasing = TRUE),]
-  
-  #getting top 5%
-  index_of_top5percent = c(1:(dim(sortedVarValues)[1]*.05))
-  top_5percent_var = sortedVarValues[index_of_top5percent,]
-  
-  #concatenating the design file and the top 1000 var cg regions
-  descript_top5percent = rbind(designData, top_5percent_var)
-  
-  last_column = dim(descript_top5percent)[2] #this is the beta Variance row
-  descript_top5percent = descript_top5percent[,1:last_column -1] #removing the beta variance row
-  
-  return(descript_top5percent)
-}
 
 #Creating a function to take the top 1% variation beta values 
 top1Percent_Var <- function(df){
@@ -74,7 +45,7 @@ top1Percent_Var <- function(df){
   index_of_top1percent = c(1:(dim(sortedVarValues)[1]*.01))
   top_1percent_var = sortedVarValues[index_of_top1percent,]
   
-  #concatenating the design file and the top 1000 var cg regions
+  #concatenating the design file and the top 1% variable beta values
   descript_top1percent = rbind(designData, top_1percent_var)
   
   last_column = dim(descript_top1percent)[2] #this is the beta Variance row
@@ -112,4 +83,66 @@ write.csv(top5PerVar_vsd,
 write.csv(top1PerVar_vsd, 
           'C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\RNA_seq_design_info_with_vsd_values_Top1Percent_Patient1_9Feb2023.csv')
 
+
+
+
+
+#_____________________________________________Creating Clustvis df____________________________________#
+
+#reading in the file and processing it so that it is the proper format
+tdata <- read.csv('C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\Pt5_vsd.csv')
+rownames(tdata) <- make.names(tdata[,1], unique = TRUE) #issue when setting rowname using csv
+tdata <- tdata[,2: dim(tdata)[2]] #removing the first column
+designData <- read.csv('C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\design_pt5.csv', row.names = 1)
+
+designData <- t(designData)
+
+clustvis_format = rbind(designData, tdata)
+clustvis_format = clustvis_format [c(1,c(3, c(4:dim(clustvis_format)[1]))),]
+
+
+#_______________________________________________selecting data of interest___________________________________#
+
+
+vsd_with_variance <- CalVar(clustvis_format)
+top5PerVar_vsd <- top5Percent_Var(vsd_with_variance)
+top1PerVar_vsd <- top1Percent_Var(vsd_with_variance )
+
+#writing dataframe to csv
+write.csv(top5PerVar_vsd, 
+          'C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\RNA_seq_design_info_with_vsd_values_Top5Percent_Patient5_9Feb2023.csv')
+
+write.csv(top1PerVar_vsd, 
+          'C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\RNA_seq_design_info_with_vsd_values_Top1Percent_Patient5_9Feb2023.csv')
+
+
+
+
+#_____________________________________________Creating Clustvis df____________________________________#
+
+#reading in the file and processing it so that it is the proper format
+tdata <- read.csv('C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\Pt8_vsd.csv')
+rownames(tdata) <- make.names(tdata[,1], unique = TRUE) #issue when setting rowname using csv
+tdata <- tdata[,2: dim(tdata)[2]] #removing the first column
+designData <- read.csv('C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\design_pt8.csv', row.names = 1)
+
+designData <- t(designData)
+
+clustvis_format = rbind(designData, tdata)
+clustvis_format = clustvis_format [c(1,c(3, c(4:dim(clustvis_format)[1]))),]
+
+
+#_______________________________________________selecting data of interest___________________________________#
+
+
+vsd_with_variance <- CalVar(clustvis_format)
+top5PerVar_vsd <- top5Percent_Var(vsd_with_variance)
+top1PerVar_vsd <- top1Percent_Var(vsd_with_variance )
+
+#writing dataframe to csv
+write.csv(top5PerVar_vsd, 
+          'C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\RNA_seq_design_info_with_vsd_values_Top5Percent_Patient8_9Feb2023.csv')
+
+write.csv(top1PerVar_vsd, 
+          'C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\RNA_seq\\RNA_seq_design_info_with_vsd_values_Top1Percent_Patient8_9Feb2023.csv')
 
