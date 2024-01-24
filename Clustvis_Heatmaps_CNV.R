6221838e0924daf3a0590b6889c5a9ea87efa65c
przybyllab <146016089+przybyllab@users.noreply.github.com>
2024-01-24 10:03:54 -0600

    Update Clustvis_Heatmaps_CNV.R

--- Clustvis_Heatmaps_CNV.R
+++ Clustvis_Heatmaps_CNV.R
@@ -15,7 +15,7 @@ for (clusterType in c("complete","average","mcquitty", "ward.D")){
   for (merge_dvg in c( "_nomerge_dvgPartialNone")){
     
     #reading files for patient 5
-    file = sprintf("C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\CNV\\clustvis_format\\clustvisPatient5_CNVs_filtered%s.csv", merge_dvg)
+    file = sprintf("[Insert pathway to cnv file]\\clustvisPatient5_CNVs_filtered%s.csv", merge_dvg)
     
     #preparing data for clustvis 
     imp = importData(file, nbrRowAnnos = 0, nbrColAnnos = 5)
@@ -24,17 +24,17 @@ for (clusterType in c("complete","average","mcquitty", "ward.D")){
     #heatmap generation
     hm = generateHeatmap(proc, clustDistRows = NA, clustMethodRows = "ward.D", clustDistCols = "euclidean", 
                          clustMethodCols = clusterType, showRownames= F, colorAnnoCol = c("Type", "Mutation","Year"), legendColorScheme =  "Set2", colorRangeMax = 0.5, colorRangeMin = -0.5)
-    saveHeatmap(hm, file = sprintf("C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\Exploratory_Analysis\\Patient5\\CNV\\Heatmap\\clustvisHeatmap_CNV_%s_20DTF_NOrowCenter_NOscalling_%s_Patient5_%s_clustvis_%s.pdf",clsuterType, merge_dvg,Date, Package_version))
+    saveHeatmap(hm, file = sprintf("[Pathway to here to save cnv heatmaps]\\clustvisHeatmap_CNV_%s_20DTF_NOrowCenter_NOscalling_%s_Patient5_%s_clustvis_%s.pdf",clsuterType, merge_dvg,Date, Package_version))
     
     #reading files for patient 8 
-    file = sprintf("C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\CNV\\clustvis_format\\clustvisPatient8_CNVs_filtered%s.csv", merge_dvg)
+    file = sprintf("[Insert pathway to cnv file]\\clustvisPatient8_CNVs_filtered%s.csv", merge_dvg)
     imp = importData(file, nbrRowAnnos = 0, nbrColAnnos = 5)
     proc = processData(imp, rowScaling = "none", rowCentering = FALSE)
     
     #heatmap generation
     hm = generateHeatmap(proc, clustDistRows = NA, clustMethodRows = "ward.D", clustDistCols = "euclidean", 
                          clustMethodCols = clusterType, showRownames= F, colorAnnoCol = c("Type", "Mutation","Year"),legendColorScheme =  "Set2",colorRangeMax = 0.5, colorRangeMin = -0.5)
-    saveHeatmap(hm, file = sprintf("C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\Exploratory_Analysis\\Patient8\\CNV\\Heatmap\\clustvisHeatmap_CNV_%s_20DTF_NOrowCenter_NOscalling_%s_Patient8_%s_clustvis_%s.pdf",clsuterType, merge_dvg,Date, Package_version))
+    saveHeatmap(hm, file = sprintf("[Pathway to here to save cnv heatmaps]\\clustvisHeatmap_CNV_%s_20DTF_NOrowCenter_NOscalling_%s_Patient8_%s_clustvis_%s.pdf",clsuterType, merge_dvg,Date, Package_version))
   }
 }
 
@@ -49,7 +49,7 @@ for (clsuterType in c("complete","average","mcquitty", "ward.D")){
   for (merge_dvg in c("_nomerge_dvgNotConsidered")){
   
     #reading files for patient 1
-    file = sprintf("C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\CNV\\clustvis_format\\clustvisPatient1_CNVs_filtered%s.csv", merge_dvg)
+    file = sprintf("[Insert pathway to cnv file]\\clustvisPatient1_CNVs_filtered%s.csv", merge_dvg)
     
     #preparing data for clustvis 
     imp = importData(file, nbrRowAnnos = 0, nbrColAnnos = 5)
@@ -58,7 +58,7 @@ for (clsuterType in c("complete","average","mcquitty", "ward.D")){
     #heatmap generation
     hm = generateHeatmap(proc, clustDistRows = NA, clustMethodRows = "ward.D", clustDistCols = "euclidean", 
                          clustMethodCols = clsuterType,showRownames= F, colorAnnoCol = c("Type", "Mutation","Year"), legendColorScheme =  "Set2", colorRangeMax = 0.8, colorRangeMin = -0.8)
-    saveHeatmap(hm, file = sprintf("C:\\Users\\chels\\OneDrive\\Documents\\Pryzbyl_Lab\\Exploratory_Analysis\\Patient1\\CNV\\Heatmap\\clustvisHeatmap_CNV_%s_20DTF_NOrowCenter_NOscalling_%s_Patient1_%s_clustvis_%s.pdf",clsuterType, merge_dvg,Date, Package_version))
+    saveHeatmap(hm, file = sprintf("[Pathway to here to save cnv heatmaps]\\clustvisHeatmap_CNV_%s_20DTF_NOrowCenter_NOscalling_%s_Patient1_%s_clustvis_%s.pdf",clsuterType, merge_dvg,Date, Package_version))
   }
 }
 
